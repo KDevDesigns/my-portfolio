3434c1a0f2b865d86b683d112717fe85345bf8d1
Kevin Kurt Intong <kevinkurt1422@gmail.com>
2025-09-23 19:21:16 +0800

    New Update

--- src/app/casestudy/CicadaHills/page.tsx
+++ src/app/casestudy/CicadaHills/page.tsx
@@ -10,6 +10,8 @@ import PrototypeStep2 from "@/lib/Cicada Hills/Wireframes/PrototypeStep2.svg";
 import PrototypeStep3 from "@/lib/Cicada Hills/Wireframes/PrototypeStep3.svg";
 import PrototypeStep4 from "@/lib/Cicada Hills/Wireframes/PrototypeStep4.svg";
 import PrototypeStep5 from "@/lib/Cicada Hills/Wireframes/PrototypeStep5.svg";
+import NewUserflow from "@/lib/Cicada Hills/Research/NewUserflow.svg"
+import OldUserflow from "@/lib/Cicada Hills/Research/Userflow.svg";
 import { Inter } from "next/font/google";
 
 // Import Framer Motion
@@ -40,7 +42,7 @@ export default function CicadaHills({}) {
     hidden: { opacity: 0, scale: 0.8 },
     show: { opacity: 1, scale: 1, transition: { duration: 0.8 } },
   };
-  
+
   const textVariants = {
     hidden: { opacity: 0, y: 20 },
     show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
@@ -62,25 +64,17 @@ export default function CicadaHills({}) {
             transition={{ duration: 0.8, delay: 0.5 }}
             className="text-4xl lg:text-6xl my-4 font-semibold text-center text-teal-700"
           >
-            <span className="text-black font-bold">Case Study:</span> From
-            Messenger Chaos to Seamless Booking at Cicada Hills
+            <span className="text-black font-bold">Case Study:</span>{" "}
+            Reimagining Cicada Hills' Digital Experience
           </motion.h1>
-          <motion.p
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: 0.7 }}
-            className="text-center lg:text-2xl md:text-xl text-xl"
-          >
-            From Messenger Chaos to Seamless Booking at Cicada Hills
-          </motion.p>
         </motion.div>
         {/* Stats Section */}
-        <motion.div
+        {/* <motion.div
           variants={containerVariants}
           initial="hidden"
           whileInView="show"
           viewport={{ once: true }}
-          className="flex flex-col gap-16 lg:flex-row md:flex-row mx-auto px-16"
+          className="flex flex-col gap-16 lg:flex-row md:flex-row mx-auto px-16 py-12"
         >
           <motion.div variants={itemVariants} className="text-center grow">
             <h1 className="text-xl lg:text-2xl font-medium">Timeframe</h1>
@@ -94,7 +88,7 @@ export default function CicadaHills({}) {
             <h1 className="text-xl lg:text-2xl font-medium">Tools</h1>
             <p className="text-lg">Figma</p>
           </motion.div>
-        </motion.div>
+        </motion.div> */}
         {/* Main Image */}
         <motion.div
           initial={{ opacity: 0 }}
@@ -120,33 +114,67 @@ export default function CicadaHills({}) {
         >
           <motion.div variants={itemVariants} className="flex-1/2">
             <h1 className="text-3xl text-teal-700 font-medium ">
-              Introduction
+              Project Overview
             </h1>
             <p className="italic text-sm">Setting the stage for change</p>
             <p className="text-justify my-4">
-              Cicada Hills is a nature-inspired events venue in Davao City,
-              hosting weddings, retreats, and milestone celebrations. While the
-              venue itself impressed visitors, the booking process—managed only
-              through Facebook Messenger and phone calls—was unreliable. Guests
-              often felt uncertain, and the owner lost time managing scattered
-              conversations instead of growing the business.
+              Cicada Hills, a stunning nature-inspired events venue in Davao
+              City, had cultivated a strong following on social media,
+              showcasing its beautiful scenery and premium offerings. However,
+              this popular online presence masked a significant operational
+              weakness: the business relied entirely on a manual, high-friction
+              booking process through Facebook Messenger, which created
+              operational bottlenecks and a frustrating experience for a growing
+              customer base.
+            </p>
+            <p className="text-justify my-4">
+              The co-owner approached me with a clear challenge: to lead a
+              complete digital transformation that would elevate their brand
+              beyond social media. The project involved designing a seamless
+              online booking system, creating a professional website to capture
+              organic search traffic, and executing a full brand identity
+              refresh to build credibility and trust.
             </p>
           </motion.div>
           <motion.div variants={itemVariants} className="flex-1/2">
-            <h1 className="text-3xl text-teal-700 font-medium ">The Spark</h1>
-            <p className="italic text-sm">
-              The vision that set the project in motion
-            </p>
+            <h1 className="text-3xl text-teal-700 font-medium ">
+              The Challenge
+            </h1>
+            <p className="italic text-sm">The Manual Process Dilemma</p>
             <p className="text-justify my-4">
-              The owner wanted a professional website that would be simple,
-              trustworthy, and polished. The solution: a stepper booking
-              system—a guided process easy enough for any tech level. Alongside
-              this, the owner requested a logo rebrand to capture Cicada Hills’
-              natural elegance and strengthen its identity across all
-              touchpoints.
+              During my initial interview with the co-owner, she detailed a
+              workflow that was both time-consuming and out of sync with the
+              venue's premium image. The core problem was a disconnect between
+              the high-quality experience offered on-site and the clunky,
+              inefficient process customers faced online.
             </p>
+            <ul className="list-disc flex flex-col  list-inside gap-2 my-4">
+              The co-owner’s goals were clear:
+              <li>Expand visibility beyond Facebook.</li>
+              <li>Build a credible, professional online presence.</li>
+              <li>Automate operations to save time and reduce errors.</li>
+            </ul>
           </motion.div>
         </motion.div>
+        <Separator className="my-1 border-1 w-6xl mx-auto" />
+        <div className="p-16 lg:px-32">
+          {/* */}
+          <motion.div
+            variants={textVariants}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.5 }}
+          >
+            <h1 className="text-3xl text-teal-700 font-medium text-center my-8">
+              My Role
+            </h1>
+            <p className="text-center my-8">
+              I acted as the UI/UX Designer for this project. While new to
+              formal research, I leaned heavily on UI design expertise to craft
+              a system that felt professional, trustworthy, and easy to use.
+            </p>
+          </motion.div>
+        </div>
         <Separator className="my-1 border-1 w-6xl mx-auto" />
         {/* Pain Points Section */}
         <div className="p-16 lg:px-32">
@@ -156,23 +184,29 @@ export default function CicadaHills({}) {
             whileInView="show"
             viewport={{ once: true, amount: 0.5 }}
           >
-            <h1 className="text-3xl text-teal-700 font-medium text-center my-8">
-              Understanding the Users
+            <h1 className="text-3xl text-teal-700 font-medium text-start my-8">
+              Research & Discovery
             </h1>
-            <p className="text-center my-8">
-              Research involved interviews with the owner and one staff member,
-              plus collected guest feedback from reviews and comments. While
-              cost was expected to be the main concern, it became clear that
-              convenience and trust mattered more. Guests wanted certainty,
-              transparency, and proof that their booking was secure.
+            <p className="text-justify my-8">
+              As part of discovery, I mapped the venue’s existing workflow to
+              identify pain points and inefficiencies. The assessment revealed a
+              highly manual, fragmented system:
             </p>
+          </motion.div>
+          <motion.div>
+            <Image src={OldUserflow} alt={""} />
+          </motion.div>
+          <motion.div>
+            <h1 className="text-3xl text-teal-700 font-medium text-center my-8">
+              Pain Points
+            </h1>
           </motion.div>
           <motion.div
             variants={containerVariants}
             initial="hidden"
             whileInView="show"
             viewport={{ once: true }}
-            className="flex flex-col lg:flex-row gap-12 my-32"
+            className="flex flex-col lg:flex-row gap-12 my-24"
           >
             <motion.div
               variants={itemVariants}
@@ -180,10 +214,11 @@ export default function CicadaHills({}) {
             >
               <CalendarX2 className="text-teal-700" />
               <h1 className="text-center text-lg font-semibold text-teal-700">
-                Unclear availability
+                Operational inefficiency
               </h1>
               <p className="text-center">
-                Users couldn’t tell if dates were actually free.
+                staff wasted hours on admin tasks instead of client-facing
+                service.
               </p>
             </motion.div>
             <motion.div
@@ -192,10 +227,11 @@ export default function CicadaHills({}) {
             >
               <Snail className="text-teal-700" />
               <h1 className="text-center text-lg font-semibold text-teal-700">
-                Slow confirmations
+                Human error risks
               </h1>
               <p className="text-center">
-                Waiting hours—or days—for replies felt endless.
+                misspelled details, double-bookings, and lost inquiries in
+                Messenger.
               </p>
             </motion.div>
             <motion.div
@@ -204,9 +240,11 @@ export default function CicadaHills({}) {
             >
               <CircleQuestionMark className="text-teal-700" />
               <h1 className="text-center text-lg font-semibold text-teal-700">
-                Confusing process
+                Limited growth
               </h1>
-              <p className="text-center">Each inquiry started from scratch.</p>
+              <p className="text-center">
+                visibility stuck inside Facebook; no reach on search.
+              </p>
             </motion.div>
             <motion.div
               variants={itemVariants}
@@ -214,14 +252,15 @@ export default function CicadaHills({}) {
             >
               <ShieldOff className="text-teal-700" />
               <h1 className="text-center text-lg font-semibold text-teal-700">
-                Lack of trust
+                Customer frustration
               </h1>
               <p className="text-center">
-                Without proof of booking, users doubted they were secure.
+                long wait times, no transparency on dates or packages,
+                unprofessional payment experience.
               </p>
             </motion.div>
           </motion.div>
-          <motion.div
+          {/* <motion.div
             variants={textVariants}
             initial="hidden"
             whileInView="show"
@@ -230,8 +269,11 @@ export default function CicadaHills({}) {
             <h1 className="text-3xl text-teal-700 font-medium text-center">
               Personas
             </h1>
-            <Image className="my-6" src={ImagePlaceholder} alt={""}></Image>
-          </motion.div>
+            <motion.div className="py-12 flex flex-col gap-8 md:justify-center md:items-center">
+              <Image className="w-full md:w-1/1" src={Hannah} alt={""}></Image>
+              <Image className="w-full md:w-1/1" src={Hannah} alt={""}></Image>
+            </motion.div>
+          </motion.div> */}
         </div>
         <Separator className="my-1 border-1 w-6xl mx-auto" />
         {/* Wireframes Section */}
@@ -272,7 +314,7 @@ export default function CicadaHills({}) {
                 highlighted packages more clearly, and built trust by addressing
                 concerns about hidden charges.
               </motion.p>
-              <Image src={ImagePlaceholder} alt={"User flow diagram"} />
+              <Image src={NewUserflow} alt={"User flow diagram"} />
             </motion.div>
 
             <motion.div
@@ -538,9 +580,15 @@ export default function CicadaHills({}) {
             viewport={{ once: true, amount: 0.5 }}
             className="indent-8 list-disc list-inside my-4 flex flex-col gap-2"
           >
-            <motion.li variants={itemVariants}>Training staff to use the system</motion.li>
-            <motion.li variants={itemVariants}>Supporting guests who still prefer Messenger</motion.li>
-            <motion.li variants={itemVariants}>Ensuring stable internet access at the venue</motion.li>
+            <motion.li variants={itemVariants}>
+              Training staff to use the system
+            </motion.li>
+            <motion.li variants={itemVariants}>
+              Supporting guests who still prefer Messenger
+            </motion.li>
+            <motion.li variants={itemVariants}>
+              Ensuring stable internet access at the venue
+            </motion.li>
           </motion.ul>
           <motion.p
             initial={{ opacity: 0, y: 20 }}
@@ -570,10 +618,22 @@ export default function CicadaHills({}) {
             className="flex flex-col md:flex-row gap-12"
           >
             <motion.div variants={itemVariants} className="flex-1/2">
-              <Card><Image src={ImagePlaceholder} alt={""}/><CardTitle className="px-4 text-2xl">MindSpace</CardTitle><CardDescription className="px-4">Mobile App Case Study</CardDescription></Card>
+              <Card>
+                <Image src={ImagePlaceholder} alt={""} />
+                <CardTitle className="px-4 text-2xl">MindSpace</CardTitle>
+                <CardDescription className="px-4">
+                  Mobile App Case Study
+                </CardDescription>
+              </Card>
             </motion.div>
             <motion.div variants={itemVariants} className="flex-1/2">
-              <Card><Image src={ImagePlaceholder} alt={""}/><CardTitle className="px-4 text-2xl">MindSpace</CardTitle><CardDescription className="px-4">Mobile App Case Study</CardDescription></Card>
+              <Card>
+                <Image src={ImagePlaceholder} alt={""} />
+                <CardTitle className="px-4 text-2xl">MindSpace</CardTitle>
+                <CardDescription className="px-4">
+                  Mobile App Case Study
+                </CardDescription>
+              </Card>
             </motion.div>
           </motion.div>
         </div>
